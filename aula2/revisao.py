1606a8874e48d150d618e100105bc1027737bbdf
barbosafernanda <fervieirabarbosa@gmail.com>
2025-08-30 09:57:03 -0300

    estruração de pastas

---
+++ aula2/revisao.py
@@ -0,0 +1,10 @@
+print("Bem vindo ao siostema!")
+print("Digite se você é maior de idade")
+valor = input("Digite 1 para maior e 0 para menor:")
+
+if valor == 1:
+    print("Você é maior de idade!")
+elif valor == 0:
+    print("Você é menor de idade!")
+else: 
+    print("É uma opção inválidada!")
